87021a15309773eb0e4fd21d0d5206dfe0b2c718
Tien Nguyen <neitomic@gmail.com>
2024-06-06 11:34:40 +0700

    wip: tls key gen and server

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,46 @@
 pub mod dns;
 pub mod http;
+pub mod tls;
+
+use std::net::SocketAddr;
+use std::path::{Path, PathBuf};
 
 use axum::routing::post;
 use axum::{
     body::Bytes, extract::Query, http::HeaderMap, response::IntoResponse, routing::get, Router,
 };
+use axum_server::tls_rustls::RustlsConfig;
 use dns::record::DnsPacket;
 use dns::{buffer::BytePacketBuffer, recursive_lookup};
 use http::{DnsQueryParams, DnsResponse};
 
 #[tokio::main]
 async fn main() {
+    let dir = PathBuf::from("test");
+    let cert: tls::Cert;
+
+    if let Some(existing_cert) = tls::Cert::load_if_exists(dir.as_path(), "name").unwrap() {
+        cert = existing_cert;
+    } else {
+        let ca = tls::generate_ca("country", "organization").unwrap();
+        cert = tls::generate_cert(&ca, "cn", vec![]).unwrap();
+    }
+
+    let config = RustlsConfig::from_pem(cert.cert_pem().into_bytes(), cert.key_pem().into_bytes())
+        .await
+        .unwrap();
+
     let app = Router::new()
         .route("/dns-query", get(handle_get))
         .route("/dns-query", post(handle_post));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    // run https server
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum_server::bind_rustls(addr, config)
+        .serve(app.into_make_service())
         .await
         .unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
 }
 
 async fn handle_get(headers: HeaderMap, Query(params): Query<DnsQueryParams>) -> impl IntoResponse {
---
+++ src/tls/mod.rs
@@ -0,0 +1,164 @@
+use std::{
+    fs::{self, File},
+    path::{Path, PathBuf},
+};
+
+use rcgen::{
+    BasicConstraints, Certificate, CertificateParams, DistinguishedName, DnType, IsCa, KeyPair,
+    KeyUsagePurpose, SanType,
+};
+use ring::{rand::SystemRandom, signature::Ed25519KeyPair};
+
+pub struct Cert {
+    cert_pem: String,
+    key_pair: KeyPair,
+}
+
+impl Cert {
+    pub fn write(&self, dir: &Path, name: &str) -> anyhow::Result<()> {
+        use std::io::Write;
+        std::fs::create_dir_all(dir)?; //maybe dangerous?
+
+        let key_path = dir.join(format!("{name}.key.pem"));
+        let mut key_out = File::create(key_path)?;
+        write!(key_out, "{}", self.key_pair.serialize_pem())?;
+
+        let cert_path = dir.join(format!("{name}.pem"));
+        let mut cert_out = File::create(cert_path)?;
+        write!(cert_out, "{}", self.cert_pem)?;
+
+        Ok(())
+    }
+
+    pub fn self_signed_cert(&self) -> anyhow::Result<Certificate> {
+        let params = CertificateParams::from_ca_cert_pem(&self.cert_pem)?;
+        let cert = params.self_signed(&self.key_pair)?;
+        Ok(cert)
+    }
+
+    pub fn cert_pem(&self) -> String {
+        self.cert_pem.clone()
+    }
+    pub fn key_pem(&self) -> String {
+        self.key_pair.serialize_pem()
+    }
+
+    pub fn load(dir: &Path, name: &str) -> anyhow::Result<Cert> {
+        let key_path = dir.join(format!("{name}.key.pem"));
+        let cert_path = dir.join(format!("{name}.pem"));
+
+        Self::load_from_file(key_path, cert_path)
+    }
+
+    fn load_from_file(key_path: PathBuf, cert_path: PathBuf) -> anyhow::Result<Cert> {
+        let key_pem = fs::read_to_string(key_path)?;
+        let cert_pem = fs::read_to_string(cert_path)?;
+
+        let key_pair = KeyPair::from_pem(&key_pem)?;
+        Ok(Cert { cert_pem, key_pair })
+    }
+
+    pub fn load_if_exists(dir: &Path, name: &str) -> anyhow::Result<Option<Cert>> {
+        let key_path = dir.join(format!("{name}.key.pem"));
+        let cert_path = dir.join(format!("{name}.pem"));
+
+        if key_path.exists() && cert_path.exists() {
+            let cert = Self::load_from_file(key_path, cert_path)?;
+            Ok(Some(cert))
+        } else if key_path.exists() || cert_path.exists() {
+            Err(anyhow::anyhow!("errr"))
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+pub fn generate_ca(country: &str, organization: &str) -> anyhow::Result<Cert> {
+    let mut params = CertificateParams::default();
+    params.distinguished_name = DistinguishedName::new();
+    params.is_ca = IsCa::Ca(BasicConstraints::Unconstrained);
+    params.key_usages.push(KeyUsagePurpose::DigitalSignature);
+    params.key_usages.push(KeyUsagePurpose::KeyCertSign);
+    params.key_usages.push(KeyUsagePurpose::CrlSign);
+
+    params.distinguished_name.push(DnType::CountryName, country);
+    params
+        .distinguished_name
+        .push(DnType::OrganizationName, organization);
+
+    let rng = SystemRandom::new();
+    let alg = &rcgen::PKCS_RSA_SHA256;
+    let pkcs8_bytes =
+        Ed25519KeyPair::generate_pkcs8(&rng).or(Err(rcgen::Error::RingUnspecified))?;
+    let key_pair: KeyPair =
+        KeyPair::from_pkcs8_der_and_sign_algo(&pkcs8_bytes.as_ref().into(), alg)?;
+
+    let cert: Certificate = params.self_signed(&key_pair)?;
+    let cert_pem = cert.pem();
+    Ok(Cert { cert_pem, key_pair })
+}
+
+pub fn generate_cert(ca: &Cert, cn: &str, sans: Vec<SanType>) -> anyhow::Result<Cert> {
+    let mut params = CertificateParams::default();
+    params.distinguished_name = DistinguishedName::new();
+    params.is_ca = IsCa::NoCa;
+    params.use_authority_key_identifier_extension = true;
+    params.key_usages.push(KeyUsagePurpose::DigitalSignature);
+    params.distinguished_name.push(DnType::CommonName, cn);
+    params.subject_alt_names.extend(sans); // todo: handle empty sans
+
+    let rng = SystemRandom::new();
+    let alg = &rcgen::PKCS_RSA_SHA256;
+    let pkcs8_bytes =
+        Ed25519KeyPair::generate_pkcs8(&rng).or(Err(rcgen::Error::RingUnspecified))?;
+    let key_pair: KeyPair =
+        KeyPair::from_pkcs8_der_and_sign_algo(&pkcs8_bytes.as_ref().into(), alg)?;
+    let ca_cert = ca.self_signed_cert()?;
+    let cert = params.signed_by(&key_pair, &ca_cert, &ca.key_pair)?;
+    let cert_pem = cert.pem();
+    Ok(Cert { cert_pem, key_pair })
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_gen_and_load_ca() {
+        let ca = generate_ca("VN", "Example").unwrap();
+
+        let dir = tempfile::tempdir().unwrap();
+        let path = dir.into_path();
+        ca.write(path.as_path(), "unit_test_ca").unwrap();
+
+        let loaded_ca = Cert::load(path.as_path(), "unit_test_ca").unwrap();
+        assert_eq!(ca.cert_pem, loaded_ca.cert_pem);
+        assert_eq!(
+            ca.key_pair.serialize_pem(),
+            loaded_ca.key_pair.serialize_pem()
+        );
+    }
+
+    #[test]
+    fn test_gen_and_load_cert() {
+        let ca = generate_ca("VN", "Example").unwrap();
+        let cert = generate_cert(
+            &ca,
+            "example.com",
+            vec![SanType::DnsName("localhost".try_into().unwrap())],
+        )
+        .unwrap();
+
+        let dir = tempfile::tempdir().unwrap();
+        let path = dir.into_path();
+        cert.write(path.as_path(), "unit_test_cert").unwrap();
+
+        let loaded_cert = Cert::load(path.as_path(), "unit_test_cert").unwrap();
+        assert_eq!(cert.cert_pem, loaded_cert.cert_pem);
+        assert_eq!(
+            cert.key_pair.serialize_pem(),
+            loaded_cert.key_pair.serialize_pem()
+        );
+    }
+}
